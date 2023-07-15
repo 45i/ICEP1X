4c310838bd7fd869ab61d3fa8d55da59af870c6a
45i <86361659+45i@users.noreply.github.com>
2023-07-15 12:13:06 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,22 +16,23 @@ try:
 # Send a GET request to the API endpoint
     response = requests.get(url)
     # print(response.json())
-    
+    log=""
     # Check if the request was successful
     if response.status_code == 200:
         # Get the list of files from the response
         files = response.json()
-    
+        print("Files Found: ")
         # Iterate over every file in the repository
         for file in files:
-         print(file["name"])
+            if file["name"].endswith(".py"):
+                print(file["name"])
+        for file in files:
             # Check if the file is a Python file
          if file["name"].endswith(".py"):
                 # Get the content of the file from the response
             uri = file["html_url"]
             res = requests.get(uri)
-            print(res.json()) 
-            print()
+            
             if res.status_code == 200:
         # Get the content of the file from the response
                 content = res.json()["payload"]["blob"]["rawLines"]
@@ -44,17 +45,19 @@ try:
                 # Check if the local file exists
                 if os.path.exists(local_path):
                     # Read the contents of the local file
-                    with open(local_path, "r") as f:
-                        local_content = f.read()
-    
+                    with open(local_path, "r+") as f:
+                        local_content = f.read().split("\n")
+                    print(local_content)
                     # Check if the local file is up-to-date
                     if local_content == content:
                         print(f"{local_path} is up-to-date")
+                        log+=f"{local_path} is up-to-date\n"
                     else:
                         # Write the contents of the remote file to the local file
                         with open(local_path, "w+") as f:
                             f.writelines(content)
                         print(f"{local_path} updated")
+                        log+=f"{local_path} updated\n"
                 else:
                     # Create the local directory if it does not exist
                     os.makedirs(os.path.dirname(local_path), exist_ok=True)
@@ -63,10 +66,14 @@ try:
                     with open(local_path, "w+") as f:
                         f.writelines(content)
                     print(f"{local_path} created")
+                    log+=f"{local_path} created\n"
+        showAlert(f"Auto-Update Completed Successfully!\n{log}","Great!")
     else:
         # Print an error message if the request was not successful
-        showAlert(f"An Exception Occurred! Auto-Update Failed! \nException Details: {e} \nOpening Editor...")
+        print(f"Error: {response.status_code} - {response.json()['message']}")
+        showAlert(f"Error: {response.status_code} - {response.json()['message']}","Ok")
     RunMainLoop()
 except Exception as e :
-    showAlert(f"An Exception Occurred! Auto-Update Failed! \nException Details: {e} \nOpening Editor...")
+    print(f"An Exception Occurred! Auto-Update Failed! \n{e} \nOpening Editor...")
+    showAlert(f"An Exception Occurred! Auto-Update Failed! \n{e} \nOpening Editor...")
     RunMainLoop()
